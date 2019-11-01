df7d9c24658397c7132171ebc46dec9a53dcb2ff
idriscahyonoo <idriscahyono@gmail.com>
2019-11-01 10:56:32 +0700

    set port heroku

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const router = require('./config/router')
-require('./config/db')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const router = require("./config/router");
+require("./config/db");
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.get("/", function(req, res) {
+  res.send("Hello Word");
+});
 
-app.get('/', function (req, res) {
-    res.send('Hello Word')
-})
+app.use(router);
 
-app.use(router)
-app.listen(3000, function () {
-    console.log('Server Running')
-})
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Server Running");
+});
